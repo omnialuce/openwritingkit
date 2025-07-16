2855034d1eaf98dc490821a3ec6dbe917636463a
Luana Aires <luanagarcia0111@gmail.com>
2025-07-16 06:12:04 +0000

    Still having issues with those margins, on both left and right sides of

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -40,8 +40,10 @@ export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <PublicHeader />
-      <main className="flex-1 container py-6 md:py-12">
-        {children}
+      <main className="flex-1">
+        <div className="container py-6 md:py-12">
+            {children}
+        </div>
       </main>
       <PublicFooter />
     </div>
