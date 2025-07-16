f1e5a05812b360dd96d2ef798adeb2e4e7baab3a
Luana Aires <luanagarcia0111@gmail.com>
2025-07-16 07:35:53 +0000

    Now getting the following error:
    [04:33:10.856] Running build in Washin

---
+++ vercel/path0/src/ai/dev.ts
@@ -0,0 +1,7 @@
+
+import { config } from 'dotenv';
+config();
+
+import '@/ai/flows/analyze-text-insights.ts';
+import '@/ai/flows/generate-writing-prompts.ts';
+import '@/ai/flows/get-writing-feedback.ts';
