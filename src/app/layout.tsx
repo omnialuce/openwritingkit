b829f476ff701de433d20f973c46d16e8b82dcc8
Luana Aires <luanagarcia0111@gmail.com>
2025-07-19 16:31:50 +0000

    Integration with Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SR6WBRR9F4"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          // gtag('config', 'G-SR6WBRR9F4'); // Removed as per Firebase documentation
+        </script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
